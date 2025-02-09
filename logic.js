3c1afa2271c84f7154ce8da9021c360de70e73a4
Sunil Williams <146036355+suwillia@users.noreply.github.com>
2025-02-09 15:47:42 -0500

    Add files via upload

---
+++ logic.js
@@ -0,0 +1,103 @@
+// Create the map object with center and zoom options.
+let map = L.map("map", {
+  center: [
+    40.7, -94.5
+  ],
+  zoom: 5
+});
+
+// Create the  tile layer that will be the background of our map.
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+  attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map);
+
+
+
+  //get the data
+  let url = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson"
+   
+  d3.json(url).then(
+    (data)=>{
+        console.log(data);
+        // we need the info from the 'features' key to pass to the createFeatures function
+        createFeatures(data.features);
+    }
+  );
+  // function that processes the data from the features key
+function createFeatures(earthquakeData)
+{
+  // console.log data to make sure it gets into this function
+  console.log(earthquakeData);
+
+  // function to build markers
+    function style(feature) {
+      return{
+        radius:feature.properties.mag *10,
+        fillColor:depthColor(feature.geometry.coordinates[2]),
+        color: '#FEFB01',
+        weight: 1,
+        opacity: 1,
+        fillOpacity:0.9
+      };
+    }
+
+// we need a function to adjust for color based on depth found at features.geometry.coordinates.[2]
+
+function depthColor(depth) {
+  return  depth > 90 ? '#EA8207':
+           depth > 70 ? '#bb7011' : 
+           depth > 50 ? '#c39c13' : 
+           depth > 30 ? '#b8be10' : 
+           depth > 10 ? '#7bba17' : 
+                        '#44b51b'; 
+         
+                      
+}
+
+// Function to create circle markers
+function onEachFeature(feature, layer) {
+
+  // convert timestamp to date
+  var date= new Date(feature.properties.time);
+  var realdate = date.toLocaleDateString();
+  // create popup with the data we want to show
+  layer.bindPopup(`${feature.properties.title} <br> Depth: ${feature.geometry.coordinates[2]}km <br> Date: ${realdate}`);
+}
+
+// Make the layer of earthquake data points using circle markers
+let earthquakes = L.geoJSON(earthquakeData, {
+  pointToLayer: function (feature, latlng) {
+    return L.circleMarker(latlng, style(feature));
+  },
+  onEachFeature: onEachFeature // Calls the function and binds the popups to all markers on the layer
+});
+
+// Add the earthquake data layer to the map directly
+earthquakes.addTo(map);
+
+// to add legend to map
+let legend = L.control({position: "bottomright"});
+
+// use .onAdd to add content to the legend
+legend.onAdd = function(map){
+  // .create() to make a 'div' that is going to hold the map
+  let div = L.DomUtil.create("div", "info legend"),
+    depthRanges =[-10,10,30,50,70,90],
+    labels=[];
+    // create header for label
+    let legendInfo = "<h2>Depth of Earthquake (km)</h2>"
+    div.innerHTML = legendInfo;
+  //loop through the depth intervals to create label
+  for (let i = 0; i < depthRanges.length; i++){
+    div.innerHTML +=
+        '<i style="background:' + depthColor(depthRanges[i] + 1) + '"></i> ' +
+        depthRanges[i] + (depthRanges[i + 1] ? '&ndash;' + depthRanges[i + 1] + '<br>' : '+');
+    }
+
+    return div;
+};
+
+//  add the legend to the map.
+legend.addTo(map);
+
+} 
